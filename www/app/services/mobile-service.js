10cb587015c2154e356cefe966cc618efaeaa904
vincent_minde <vincentminde@gmail.com>
2015-11-30 15:12:23 +0300

    Added camera

--- www/app/services/mobile-service.js
+++ www/app/services/mobile-service.js
@@ -5,33 +5,9 @@ var app = angular.module('app');
 app.factory('MobileService', function($q,$cordovaCamera){
     var MobileService  = {
         getPhoto: function(cameraSuccess) {
-            $cordovaCamera.getPicture().then(cameraSuccess, function(err) {
+            navigator.device.capture.captureVideo(cameraSuccess,  function(err) {
                 // error
-            });
-            this.onDeviceReady().then(function(){
-                console.log(navigator);
-                /*var options = {
-                    quality: 50,
-                    destinationType: Camera.DestinationType.DATA_URL,
-                    sourceType: Camera.PictureSourceType.CAMERA,
-                    allowEdit: true,
-                    encodingType: Camera.EncodingType.JPEG,
-                    targetWidth: 100,
-                    targetHeight: 100,
-                    popoverOptions: CameraPopoverOptions,
-                    saveToPhotoAlbum: false,
-                    correctOrientation:true
-                };*/
-
-
-            })
-        },
-        onDeviceReady:function(callBack){
-            var defer = $q.defer();
-            document.addEventListener("deviceready", function(){
-                defer.resolve();
-            }, false);
-            return defer.promise;
+            }, {limit: 1});
         }
     }
     return MobileService;
