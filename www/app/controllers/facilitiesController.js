aae27572b75d12ae1fc5df3ba727a8ebea42b8d9
vincent_minde <vincentminde@gmail.com>
2015-12-08 11:24:30 +0300

    Change configurations

--- www/app/controllers/facilitiesController.js
+++ www/app/controllers/facilitiesController.js
@@ -27,6 +27,35 @@ app.controller('FacilitiesController', function($scope,ProgramManger,MobileServi
     },function(error){
         alert("Error Getting Current Position.");
     });
+    function GoogleMap(){
+
+        this.initialize = function(){
+            var map = showMap();
+        }
+
+        var showMap = function(){
+            var mapOptions = {
+                zoom: 4,
+                center: new google.maps.LatLng(-33, 151),
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+
+            var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+
+            return map;
+        }
+    }
+    var map = new GoogleMap();
+    map.initialize();
+    document.addEventListener("deviceready", function() {
+        alert("Here");
+        var map = new GoogleMap();
+        map.initialize();
+        alert("Here3");
+    });
+
+    function onMapInit(map) {
+    }
     $http.get(DHIS2URL+'/api/organisationUnitGroups.json?paging=false&fields=:all,organisationUnits[:all]')
         .success(function(data){
             data.organisationUnitGroups.forEach(function(organisationUnitGroup){
