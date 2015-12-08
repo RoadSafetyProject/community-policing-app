b3c8b23c731c6e9b44871346608a9726476a6e53
vincent_minde <vincentminde@gmail.com>
2015-12-08 15:17:54 +0300

    Yet another test

--- www/app/app.js
+++ www/app/app.js
@@ -190,79 +190,25 @@ var app = angular.module('app', ['ui.materialize','ngRoute','uiGmapgoogle-maps']
     }])
     .controller('DateController', ["$scope",'MobileService','$http','DHIS2URL', function ($scope,MobileService,$http,DHIS2URL){
         alert("Main1");
-        var baseOptions = {
-            'maxZoom': 15,
-            'minZoom': 4,
-            'backgroundColor': '#b0d1d4',
-            'panControl': false,
-            'zoomControl': true,
-            'draggable': true,
-            'zoomControlOptions': {
-                'position': 'RIGHT_TOP',
-                'style': 'SMALL'
+        function GoogleMap(){
+
+            this.initialize = function(){
+                var map = showMap();
             }
-        };
-        alert("Main2");
-        $scope.currentPosition = {};
-        $scope.facilities = {
-            hospitals:[],
-            polices:[],
-            fire:[]
-        };
-        alert("Main3");
-        $scope.map = {center: {latitude: -6.771430, longitude: 39.239946}, options:baseOptions, zoom:8, showTraffic: true,  show: true,mapObject:{}};
 
-        alert("Main4");
-        MobileService.getGeoLocation(function(position){
-            $scope.currentPosition = position;
-        },function(error){
-            alert("Error Getting Current Position.");
-        });
-        /*function GoogleMap(){
+            var showMap = function(){
+                var mapOptions = {
+                    zoom: 4,
+                    center: new google.maps.LatLng(-33, 151),
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                }
 
-         this.initialize = function(){
-         var map = showMap();
-         }
+                var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
-         var showMap = function(){
-         var mapOptions = {
-         zoom: 4,
-         center: new google.maps.LatLng(-33, 151),
-         mapTypeId: google.maps.MapTypeId.ROADMAP
-         }
-
-         var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-
-         return map;
-         }
-         }
-         var map = new GoogleMap();
-         map.initialize();
-         document.addEventListener("deviceready", function() {
-         alert("Here");
-         var map = new GoogleMap();
-         map.initialize();
-         alert("Here3");
-         });
-
-         function onMapInit(map) {
-         }*/
-        alert("Main5");
-        $http.get(DHIS2URL+'/api/organisationUnitGroups.json?paging=false&fields=:all,organisationUnits[:all]')
-            .success(function(data){
-                data.organisationUnitGroups.forEach(function(organisationUnitGroup){
-                    if(organisationUnitGroup.name == "Hospitals"){
-                        console.log(organisationUnitGroup);
-                        organisationUnitGroup.organisationUnits.forEach(function(organisationUnit){
-                            var coordinates = eval(organisationUnit.coordinates);
-                            organisationUnit.coordinates = {"latitude":coordinates[0],"longitude":coordinates[1]};
-                            $scope.facilities.hospitals.push(organisationUnit);
-                        });
-                    }
-                });
-            })
-            .error(function(errorMessageData){
-                alert("Error Contacting server.");
-            });
+                return map;
+            }
+        }
+        var map = new GoogleMap();
+        map.initialize();
         alert("Main6");
     }]);
